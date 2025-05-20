4bbe325a6e10bf7e600a47a77ef13cbc05fbd79f
TG <mail@thomas-graefenstein.de>
2025-05-20 21:26:03 +0200

    add more example queries to trigram playground

--- trigram-demo/src/main/resources/sql-scripts/trigram-playground.sql
+++ trigram-demo/src/main/resources/sql-scripts/trigram-playground.sql
@@ -1,3 +1,21 @@
+SELECT similarity('Hello World', 'Hello World'),
+       similarity('Hello', 'Hello World'),
+       similarity('Bye World', 'Hello World'),
+       similarity('orld', 'Hello World');
+SHOW pg_trgm.similarity_threshold;
+
+SELECT word_similarity('Hello World', 'Hello World'),
+       word_similarity('Hello', 'Hello World'),
+       word_similarity('Bye World', 'Hello World'),
+       word_similarity('orld', 'Hello World');
+SHOW pg_trgm.word_similarity_threshold;
+
+SELECT strict_word_similarity('Hello World', 'Hello World'),
+       strict_word_similarity('Hello', 'Hello World'),
+       strict_word_similarity('Bye World', 'Hello World'),
+       strict_word_similarity('orld', 'Hello World');
+SHOW pg_trgm.strict_word_similarity_threshold;
+
 SELECT COUNT(*)
 FROM customer;
 
