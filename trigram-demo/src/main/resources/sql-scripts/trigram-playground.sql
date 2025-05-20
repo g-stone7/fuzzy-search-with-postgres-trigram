11ebce0c79aab22a95f12b5f49e07f09a38e10b6
TG <mail@thomas-graefenstein.de>
2025-05-20 23:11:48 +0200

    simplified examples in playground

--- trigram-demo/src/main/resources/sql-scripts/trigram-playground.sql
+++ trigram-demo/src/main/resources/sql-scripts/trigram-playground.sql
@@ -21,31 +21,15 @@ FROM customer;
 
 SELECT *
 FROM customer
-WHERE address_street ILIKE '%stitu%';
+WHERE address_city ILIKE '%arcelo%';
 
 SELECT *
 FROM customer
-WHERE 'stitu' <% address_street;
+WHERE 'arcelo' <% address_city;
 
-SELECT DISTINCT address_street,
-                word_similarity('constitucion', address_street) as score
+SELECT DISTINCT address_city,
+                word_similarity('arcelon', address_city) as score
 FROM customer
 ORDER BY score DESC;
 
-SELECT show_trgm('constitucion'), show_trgm('Constitución');
-
------
-
-CREATE EXTENSION unaccent;
-
-SELECT *
-FROM customer
-WHERE unaccent('constitucion') <% address_street;
-
-SELECT DISTINCT address_street,
-                word_similarity(unaccent('constitucion'), unaccent(address_street)) AS similarity_score
-FROM customer
-ORDER BY similarity_score DESC;
-
-SELECT unaccent('Avenida de la Constitución');
-SELECT show_trgm(unaccent('Avenida de la Constitución'));
+SELECT show_trgm('Barcelona'), show_trgm('Bárßêlònä');
